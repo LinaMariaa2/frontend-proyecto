b91cdc5ddad50961a519c07aa0affe003fcafd0c
sebastianCF <luissebastianclavijof@gmail.com>
2025-09-29 13:10:18 -0500

    listo para desplegar

--- src/app/(private)/home/admin/configuraciones/page.tsx
+++ src/app/(private)/home/admin/configuraciones/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 import React from "react";
 import Link from "next/link";
-import { User, Users, HelpCircle, LogOut, UserPlus, Settings } from "lucide-react";
+import { User, Users, LogOut, UserPlus, Settings } from "lucide-react";
 
 // --- Tipos de Datos ---
 interface ConfiguracionItem {
